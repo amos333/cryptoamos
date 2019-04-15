9910f5264aa94c894185e31704bc31db5fd97199
Amos Shukron <amos.shukron@gmail.com>
2019-04-15 10:26:12 +0300

    add cards templates

--- main.js
+++ main.js
@@ -14,8 +14,19 @@
         $($(e.relatedTarget).data("target")).removeClass('active');
     });
 
-    $.get('mainData.html', function(data) {
-    $('#home').html(data);
-  });
-    //$("#home" ).load("mainData.html" );
+    $.getJSON("https://api.coingecko.com/api/v3/coins/list", function (data) {
+        var items = [];
+        $.each(data, function (key, val) {
+            $("#home").append(
+                '<div class="card" style="width: 18rem;">' +
+                '<div class="card-body">' +
+                '<h5 class="card-title">'+ val.symbol +'</h5>' +
+                '<h6 class="card-subtitle mb-2 text-muted">'+ val.name +'</h6>' +
+                //'<p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>' +
+                //'<a href="#" class="card-link">Card link</a>' +
+                //'<a href="#" class="card-link">Another link</a>' +
+          '</div >' +
+        '</div >');
+        });
+    });
 })(jQuery);
